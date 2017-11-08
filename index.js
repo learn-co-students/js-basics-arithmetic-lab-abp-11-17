8c485e770c42afb67dfef5070f71005c458624a6
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-08 02:31:38 +0000

    Done.

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger();
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 let randomInteger = Math.round(randomNumber);
-let randomUserID = randomInteger
+let randomUserID = randomInteger + 1000000000;
